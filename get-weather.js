e25d0bb8f0bfd54f64d6086d4c413bff04f44178
Alex Ross <alxrg@me.com>
2022-10-04 22:01:07 -0500

    completed the weather api

--- get-weather.js
+++ get-weather.js
@@ -1,11 +1,10 @@
 // Functions 
-async function getWeather(apikey, zip, onSuccess, onError) {
+async function getWeather(path) {
   // Replace this with your own API key!
   // const apiKey = '467355df4c808dd6134a3b64e9ace282'
-  const apiKey = apikey
-  // const units = unit
-  const units = 'imperial'     
-  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
+  // const apiKey = apikey
+  // const units = 'imperial'     
+  // const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
   try {
     const res = await fetch(path)
     const data = await res.json()
@@ -22,14 +21,19 @@ async function getWeather(apikey, zip, onSuccess, onError) {
   }
 }
 
-export default getWeather
+async function getWeatherByZip(apiKey, zip, units) {
+  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&${units}`
+  return getWeather(path)
+}
 
-// this export default becomes a module
-// if it's a module you need to import your script in the index.html
-// <script src="get-weather.js" type="module"></script>
-{/* <script type="module">
+async function getWeatherByCity(apiKey, city, units) {
+  const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&${units}`
+  return getWeather(path)
+}
 
-import getWeather from './get-weather.js'
+async function getWeatherByGeo(apiKey, lat, lon, units) {
+  const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&${units}`
+  return getWeather(path)
+}
 
-
-</script> */}
+export default getWeatherByCity
